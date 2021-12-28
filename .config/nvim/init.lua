c911f595529bcf093385c810a3909a696cfcca08
Hunter Baxter <hunter.c.baxter@vanderbilt.edu>
2021-12-27 22:56:21 -0600

    feat: no errors yet

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,19 @@
 local modules = {
-  "keymaps",
-  "options",
-  "plugins",
-  "colorscheme",
-  "completion",
-  "lsp"
+  "user.keymaps",
+  "user.options",
+  "user.plugins",
+  "user.colorscheme",
+  "user.completion",
+  "user.lsp"
 }
 
+-- for i = 1, #modules, 1 do
+--     pcall(require, modules[i])
+-- end
+
 for i = 1, #modules, 1 do
-    pcall(require, "user." .. modules[i])
+    local status_ok, _ = pcall(require, modules[i])
+    if not status_ok then
+        vim.notify(modules[i] .. " not ok")
+    end
 end
