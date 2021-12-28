c3407d6167264b587f7f7fb0b339d404d3d0cc64
Hunter Baxter <hunter.c.baxter@vanderbilt.edu>
2021-12-27 22:44:17 -0600

    refactor: trying to fix lsp issues

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,12 @@
 local modules = {
-    "keymaps",
-    "options",
-    "plugins",
-    "colorscheme",
-    "completion"
+  "keymaps",
+  "options",
+  "plugins",
+  "colorscheme",
+  "completion",
+  "lsp"
 }
 
 for i = 1, #modules, 1 do
-    pcall(require, modules[i])
+    pcall(require, "user." .. modules[i])
 end
