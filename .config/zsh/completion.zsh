0a86a42d11fb4dbb5295ef9ac9a0a0fcfd3cc2d6
Hunter Baxter <hunter.c.baxter@vanderbilt.edu>
2022-01-15 06:53:05 -0600

    style: removed unnecessary comments

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -1,19 +1,14 @@
-# setopt NOMATCH
+# completion options
 setopt PROMPT_SUBST
 setopt AUTO_LIST # Automatically list choices on ambiguous completion
 setopt EXTENDED_GLOB # expands ~, #, ^
+setopt LIST_PACKED
+setopt AUTO_PARAM_SLASH
 
-# setopt MENU_COMPLETE
-
+# load completion
 autoload -Uz compinit; compinit;
-# zstyle => :completion:function:completer:command:argument:tag
-#
-# from learning shell scripting with zsh on oreilly
-# zstyle ':completion:*' group-name ''
-# zstyle ':completion:descriptions' format '%B%d%b'
-# zstyle ':completion:messages' format %d
-# zstyle ':completion:warnings' format 'No matches for: %d'
-# zstyle ':completion:*' completer_expand_complete_correct
 zstyle ':completion:*' menu select
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/compcache"
 zmodload zsh/complist
 _comp_options+=(globdots) # expands dotfiles
