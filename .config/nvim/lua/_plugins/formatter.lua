ac067a34c6b4a5b5841bb227a80a092d40e9f513
Hunter Baxter <hunter.c.baxter@vanderbilt.edu>
2022-01-26 10:57:00 -0600

    feat: added clang formatter

--- .config/nvim/lua/_plugins/formatter.lua
+++ .config/nvim/lua/_plugins/formatter.lua
@@ -26,5 +26,16 @@ formatter.setup {
         }
       end,
     },
+    c = {
+        -- clang-format
+       function()
+          return {
+            exe = "clang-format",
+            args = {"--assume-filename", vim.api.nvim_buf_get_name(0)},
+            stdin = true,
+            cwd = vim.fn.expand('%:p:h')  -- Run clang-format in cwd of the file.
+          }
+        end
+    },
   },
 }
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -74,7 +74,7 @@ return require("packer").startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
     -- FIXME: https://github.com/nvim-treesitter/nvim-treesitter/issues/2295
-    commit = '668de0951a36ef17016074f1120b6aacbe6c4515',
+    commit = "668de0951a36ef17016074f1120b6aacbe6c4515",
     requires = { "nvim-treesitter/nvim-treesitter-textobjects" },
     config = function()
       require "_plugins.treesitter"
